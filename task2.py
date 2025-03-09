0533836a1edcb0644cdda81f7a9b0a620d675516
Алексей <aleksej@MacBook-Pro-Aleksej.local>
2025-03-09 18:44:40 +0200

    get_numbers_ticket(min, max, quantity)

--- task2.py
+++ task2.py
@@ -12,9 +12,10 @@ def get_numbers_ticket(min, max, quantity):
     while len(unique_numbers) < quantity:
         unique_numbers.add(random.randint(min, max))
     
-    # Повертаю відсортований список чисел
+    # Повертаю відсортований список  чисел
     return sorted(list(unique_numbers))
 
 # Приклад використання функції:
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
+# Function to generate lottery numbers
