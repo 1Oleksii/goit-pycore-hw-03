cc7f1dee93218e91d18b7ab7e6616343dd2b304e
Алексей <aleksej@MacBook-Pro-Aleksej.local>
2025-03-09 18:41:23 +0200

    get_days_from_today(date)

--- task1.py
+++ task1.py
@@ -11,7 +11,7 @@ def get_days_from_today(date: str) -> int:
         # Перетворюю рядок дати у об'єкт datetime
         given_date = datetime.strptime(date, "%Y-%m-%d").date()
         
-        # Отримую поточну дату без урахування часу
+        # Отримую поточну дату без урахування  часу
         today_date = datetime.today().date()
         
         # Розраховую різницю між поточною датою і заданою
@@ -25,3 +25,4 @@ def get_days_from_today(date: str) -> int:
 
 # Приклад виклику функції
 print(get_days_from_today("2021-10-09"))  # Виводжу кількість днів від 9 жовтня 2021 до сьогодні
+# Function to calculate days from given date
